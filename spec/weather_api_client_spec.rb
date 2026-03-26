59bd743599484cb13cbd9c2d197b2f3e3de38015
Chacha-Muchacha <sdonec3@gmail.com>
2026-03-27 01:06:43 +0300

    Test: added RSpec tests for weather data parsing

--- spec/weather_api_client_spec.rb
+++ spec/weather_api_client_spec.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
-RSpec.describe WeatherApiClient do
-  it "has a version number" do
-    expect(WeatherApiClient::VERSION).not_to be nil
+require "weather_api_client"
+
+RSpec.describe WeatherApiClient::Parser do
+  let(:parser) { WeatherApiClient::Parser.new }
+
+  let(:fake_json) do
+    '{ "main": { "temp": 15.5, "humidity": 60 }, "weather": [ { "description": "ясно" } ] }'
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "успешно извлекает температуру из JSON" do
+    result = parser.parse_all_coord_city(fake_json)
+    expect(result[:temperature]).to eq(15.5)
+  end
+
+  it "успешно извлекает влажность из JSON" do
+    result = parser.parse_all_coord_city(fake_json)
+    expect(result[:humidity]).to eq(60)
+  end
+
+  it "успешно извлекает описание погоды" do
+    result = parser.parse_all_coord_city(fake_json)
+    expect(result[:condition]).to eq("ясно")
   end
 end
